46a38c9a48088cad1688d60964f8c94686220ab8
Sohail1173 <mdsohailahmed711@gmail.com>
2024-02-20 20:11:43 +0530

    requirements add

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="text-classification",
+    version="0.0.1",
+    author="Md Suhail Ahmed",
+    author_email="mdsohailahmed711@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
